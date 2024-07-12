a1a356d7be0355db16a1e025c40907de360e56c8
Rolando Retana <rperezretana@hotmail.com>
2024-07-12 11:59:04 -0700

    improvements for training

---
+++ run_continously.py
@@ -0,0 +1,19 @@
+import ctypes
+import time
+
+from CheckersTraining import CheckersTraining
+
+# Define constants
+ES_CONTINUOUS = 0x80000000
+ES_SYSTEM_REQUIRED = 0x00000001
+
+# Prevent sleep
+ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
+
+try:
+    # Your training code goes here
+    game = CheckersTraining()
+    game.run_simulation()
+finally:
+    # Restore the original state when done
+    ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
