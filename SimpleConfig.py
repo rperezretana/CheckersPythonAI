4f179fc55e1446159009190696a9b6f25f28d6e7
Rolando Retana <rperezretana@hotmail.com>
2024-07-24 18:13:18 -0700

    Update SimpleConfig.py

--- SimpleConfig.py
+++ SimpleConfig.py
@@ -9,9 +9,9 @@ PLAYER_1_ENGINE = Engines.MC
 PLAYER_2_ENGINE = Engines.RANDOM # -1
 EXECUTE_SAVE_ASYNC = False # enables multi threading to save files, sueful most of the time.
                           # To stop the proccess without damaging the file, create a file called "stop.txt", 
-                          # this will for the app to stop after the file has been saved, stopping the app safe might take a few minutes
+                          # this will ask the app to stop after the file has been saved, stopping the app safe might take a few minutes
 SAVES_INTERVAL =  100000  # this number hast to be big (>10000) if the async is enabled and debug is False.
-STOP_CHECK_INTERVAL = 3000 # this always has to be lower than SAVES_INTERVAL
+STOP_CHECK_INTERVAL = 5000 # this always has to be lower than SAVES_INTERVAL
 
     
 def debug_print(*args, end=None):
